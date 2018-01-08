46e208bdc47e204117aef060f71e49b1fb608edf
Espressif Systems <wujiangang@espressif.com>
2018-01-08 10:43:21 +0800

    fix(wifi): Fix wifi tx error issue
    wifi tx buffer should not be in iRAM.
    
    internal: 61bf6590

--- third_party/lwip/core/pbuf.c
+++ third_party/lwip/core/pbuf.c
@@ -392,6 +392,11 @@ pbuf_alloc(pbuf_layer layer, u16_t length, pbuf_type type)
   /* set flags */
   p->flags = 0;
   LWIP_DEBUGF(PBUF_DEBUG | LWIP_DBG_TRACE, ("pbuf_alloc(length=%"U16_F") == %p\n", length, (void *)p));
+
+  if ((int)p > 0x40000000) {
+      pbuf_free(p);
+      p = NULL;
+  }
   return p;
 }
 
