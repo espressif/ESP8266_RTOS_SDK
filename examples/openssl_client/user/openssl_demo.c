da52d578ad6087a14a50736023893b89f8bc9652
gfeun <32910236+gfeun@users.noreply.github.com>
2018-01-09 12:04:36 +0100

    Fix typo in example

--- examples/openssl_client/user/openssl_demo.c
+++ examples/openssl_client/user/openssl_demo.c
@@ -34,7 +34,7 @@
 
 #define OPENSSL_DEMO_THREAD_NAME "ssl_demo"
 #define OPENSSL_DEMO_THREAD_STACK_WORDS 2048
-#define OPENSSL_DEMO_THREAD_PRORIOTY 6
+#define OPENSSL_DEMO_THREAD_PRIORITY 6
 
 /*
 Fragment size range 2048~8192
@@ -232,7 +232,7 @@ void user_conn_init(void)
                       OPENSSL_DEMO_THREAD_NAME,
                       OPENSSL_DEMO_THREAD_STACK_WORDS,
                       NULL,
-                      OPENSSL_DEMO_THREAD_PRORIOTY,
+                      OPENSSL_DEMO_THREAD_PRIORITY,
                       &openssl_handle);
     if (ret != pdPASS)  {
         printf("create thread %s failed\n", OPENSSL_DEMO_THREAD_NAME);
