ae6c134ec4d5f32df2bf5e11d9f3ada6e69420ab
Wu Jian Gang <wujiangang@espressif.com>
2018-05-19 22:51:39 +0800

    feat(driver): Update to use apache license

--- components/esp8266/driver/spi_interface.c
+++ components/esp8266/driver/spi_interface.c
@@ -1,19 +1,17 @@
-/**
- *  spi_interface.c
- *
- * Defines and Macros for the SPI.
- *
- * Copyright @ 2015 Espressif System Co., Ltd.
- * All Rights Reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are NOT permitted except as agreed by
- * Espressif System Co., Ltd.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- */
+// Copyright 2018 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /**
  * @file spi_interface.c
  * @brief Defines and Macros for the SPI.
