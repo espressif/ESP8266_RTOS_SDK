6fbcd9f8239b280a9f3b746c7876122f583fa15c
Wu Jian Gang <wujiangang@espressif.com>
2018-05-19 21:13:51 +0800

    feat(lwip): Restructure lwip folder to support different platform

--- components/lwip/component.mk
+++ components/lwip/component.mk
@@ -1,10 +1,20 @@
 #
 # Component Makefile
 #
-COMPONENT_ADD_INCLUDEDIRS += lwip/src/include lwip/src/include/lwip lwip/src/include/posix lwip/include/lwip \
-								include/lwip/apps include/lwip/port apps
+COMPONENT_ADD_INCLUDEDIRS += include/lwip/apps \
+                             lwip/src/include \
+                             lwip/src/include/posix \
+                             port/esp8266/include
 
-COMPONENT_SRCDIRS += lwip/src/api lwip/src/apps/sntp lwip/src/netif lwip/src/core lwip/src/core/ipv4 lwip/src/core/ipv6 \
-								port/freertos port/netif apps/dhcpserver apps/multi-threads
+COMPONENT_SRCDIRS += apps/dhcpserver \
+                     apps/multi-threads \
+                     lwip/src/api \
+                     lwip/src/apps/sntp \
+                     lwip/src/core \
+                     lwip/src/core/ipv4 \
+                     lwip/src/core/ipv6 \
+                     lwip/src/netif \
+                     port/esp8266/freertos \
+                     port/esp8266/netif
 
 CFLAGS += -Wno-address #lots of LWIP source files evaluate macros that check address of stack variables
