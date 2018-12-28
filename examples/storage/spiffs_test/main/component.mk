735a0db08b440f510aee67781259c2a0b64a0ea1
Dong Heng <dongheng@espressif.com>
2018-12-28 11:19:25 +0800

    feat(spiffs_test): Remove SPIFFS example because it is not support

--- examples/storage/spiffs_test/main/component.mk
+++
@@ -1,9 +0,0 @@
-#
-# "main" pseudo-component makefile.
-#
-# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-
-CFLAGS += -Wno-error=implicit-function-declaration \
-		  -Wno-error=unused-label \
-		  -Wno-implicit-function-declaration \
-		  -Wno-unused-label
