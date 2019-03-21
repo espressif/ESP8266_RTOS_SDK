050fb6a63fb1e8ede5176ee54cc236b5ae727a5e
dongheng <Dong Heng>
2019-03-21 17:23:30 +0800

    feat(esp8266): Add esp-idf error number description
    Commit ID: f49e2afe

--- components/freertos/port/esp8266/panic.c
+++ components/freertos/port/esp8266/panic.c
@@ -182,9 +182,27 @@ void __attribute__((noreturn)) panicHandler(void *frame, int wdt)
     panic_info(frame, wdt);
 }
 
-void __attribute__((noreturn)) _esp_error_check_failed(esp_err_t rc, const char *file, int line, const char *function, const char *expression)
+static void esp_error_check_failed_print(const char *msg, esp_err_t rc, const char *file, int line, const char *function, const char *expression)
 {
-    printf("ESP_ERROR_CHECK failed: esp_err_t 0x%x at %p\n", rc, __builtin_return_address(0));
-    printf("file: \"%s\" line %d\nfunc: %s\nexpression: %s\n", file, line, function, expression);
+    ets_printf("%s failed: esp_err_t 0x%x", msg, rc);
+#ifdef CONFIG_ESP_ERR_TO_NAME_LOOKUP
+    ets_printf(" (%s)", esp_err_to_name(rc));
+#endif //CONFIG_ESP_ERR_TO_NAME_LOOKUP
+    ets_printf(" at 0x%08x\n", (intptr_t)__builtin_return_address(0) - 3);
+
+    // ESP8266 put main FreeRTOS code at flash
+    //if (spi_flash_cache_enabled()) { // strings may be in flash cache
+    ets_printf("file: \"%s\" line %d\nfunc: %s\nexpression: %s\n", file, line, function, expression);
+    //}
+}
+
+void _esp_error_check_failed_without_abort(esp_err_t rc, const char *file, int line, const char *function, const char *expression)
+{
+    esp_error_check_failed_print("ESP_ERROR_CHECK_WITHOUT_ABORT", rc, file, line, function, expression);
+}
+
+void _esp_error_check_failed(esp_err_t rc, const char *file, int line, const char *function, const char *expression)
+{
+    esp_error_check_failed_print("ESP_ERROR_CHECK", rc, file, line, function, expression);
     abort();
 }
