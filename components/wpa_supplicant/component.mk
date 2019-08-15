21d8877663a9615c1409147f3fcd37a4daded050
Chen Wen <chenwen@espressif.com>
2019-08-15 21:08:58 +0800

    feat(wps): add wps code to idf

--- components/wpa_supplicant/component.mk
+++ components/wpa_supplicant/component.mk
@@ -1,4 +1,4 @@
 COMPONENT_ADD_INCLUDEDIRS := include port/include
-COMPONENT_SRCDIRS := src/crypto port
+COMPONENT_SRCDIRS := src/crypto src/wps port
 
-CFLAGS += -DEMBEDDED_SUPP -D__ets__ -DESPRESSIF_USE
+CFLAGS += -DEMBEDDED_SUPP -D__ets__ -DESPRESSIF_USE -DCONFIG_WPS2 -DCONFIG_WPS_PIN -DUSE_WPS_TASK -Wno-unused-but-set-variable -Wno-empty-body
