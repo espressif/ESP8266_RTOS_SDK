653753382b91c477ce3c20a9594b058862ac26c4
Dong Heng <dongheng@espressif.com>
2020-01-07 14:35:17 +0800

    fix(esp-mqtt): fix esp-mqtt API declare error

--- components/mqtt/esp-mqtt/include/mqtt_client.h
+++ components/mqtt/esp-mqtt/include/mqtt_client.h
@@ -109,8 +109,8 @@ esp_mqtt_client_handle_t esp_mqtt_client_init(const esp_mqtt_client_config_t *co
 esp_err_t esp_mqtt_client_set_uri(esp_mqtt_client_handle_t client, const char *uri);
 esp_err_t esp_mqtt_client_start(esp_mqtt_client_handle_t client);
 esp_err_t esp_mqtt_client_stop(esp_mqtt_client_handle_t client);
-esp_err_t esp_mqtt_client_subscribe(esp_mqtt_client_handle_t client, const char *topic, int qos);
-esp_err_t esp_mqtt_client_unsubscribe(esp_mqtt_client_handle_t client, const char *topic);
+int esp_mqtt_client_subscribe(esp_mqtt_client_handle_t client, const char *topic, int qos);
+int esp_mqtt_client_unsubscribe(esp_mqtt_client_handle_t client, const char *topic);
 int esp_mqtt_client_publish(esp_mqtt_client_handle_t client, const char *topic, const char *data, int len, int qos, int retain);
 esp_err_t esp_mqtt_client_destroy(esp_mqtt_client_handle_t client);
 
