230963030d2452743a1471863b923ab78aa2f967
Dong Heng <dongheng@espressif.com>
2020-02-27 11:51:55 +0800

    feat(vfs): update vfs from esp-idf
    Commit ID: 3b16089c

--- components/lwip/port/esp8266/vfs_lwip.c
+++ components/lwip/port/esp8266/vfs_lwip.c
@@ -21,28 +21,35 @@
 #include "esp_vfs.h"
 #include "esp_vfs_dev.h"
 #include "esp_attr.h"
-#include "esp8266/uart_struct.h"
 #include "lwip/sockets.h"
 #include "sdkconfig.h"
 #include "lwip/sys.h"
 
 _Static_assert(MAX_FDS >= CONFIG_LWIP_MAX_SOCKETS, "MAX_FDS < CONFIG_LWIP_MAX_SOCKETS");
 
-static void lwip_stop_socket_select()
+static void lwip_stop_socket_select(void *sem)
 {
-    sys_sem_signal(sys_thread_sem_get()); //socket_select will return
+    sys_sem_signal(sem); //socket_select will return
 }
 
-static void lwip_stop_socket_select_isr(BaseType_t *woken)
+static void lwip_stop_socket_select_isr(void *sem, BaseType_t *woken)
 {
-    if (sys_sem_signal_isr(sys_thread_sem_get()) && woken) {
+    if (sys_sem_signal_isr(sem) && woken) {
         *woken = pdTRUE;
     }
 }
 
-static int lwip_fcntl_r_wrapper(int fd, int cmd, va_list args)
+static void *lwip_get_socket_select_semaphore(void)
 {
-    return lwip_fcntl(fd, cmd, va_arg(args, int));
+    /* Calling this from the same process as select() will ensure that the semaphore won't be allocated from
+     * ISR (lwip_stop_socket_select_isr).
+     */
+    return (void *) sys_thread_sem_get();
+}
+
+static int lwip_fcntl_r_wrapper(int fd, int cmd, int arg)
+{
+    return lwip_fcntl(fd, cmd, arg);
 }
 
 static int lwip_ioctl_r_wrapper(int fd, int cmd, va_list args)
@@ -62,6 +69,7 @@ void esp_vfs_lwip_sockets_register(void)
         .fcntl = &lwip_fcntl_r_wrapper,
         .ioctl = &lwip_ioctl_r_wrapper,
         .socket_select = &lwip_select,
+        .get_socket_select_semaphore = &lwip_get_socket_select_semaphore,
         .stop_socket_select = &lwip_stop_socket_select,
         .stop_socket_select_isr = &lwip_stop_socket_select_isr,
     };
