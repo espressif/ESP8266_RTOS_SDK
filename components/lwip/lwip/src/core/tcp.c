417a6078a8168951a0a9e2b69996409c7bc65ef7
yuanjm <yuanjianmin@espressif.com>
2020-03-26 11:58:18 +0800

    feat(lwip): Add kconfig to congigure initial RTO time and set default value to 1000

--- components/lwip/lwip/src/core/tcp.c
+++ components/lwip/lwip/src/core/tcp.c
@@ -1630,8 +1630,9 @@ tcp_alloc(u8_t prio)
     /* As initial send MSS, we use TCP_MSS but limit it to 536.
        The send MSS is updated when an MSS option is received. */
     pcb->mss = INITIAL_MSS;
-    pcb->rto = 3000 / TCP_SLOW_INTERVAL;
-    pcb->sv = 3000 / TCP_SLOW_INTERVAL;
+    /* make TCP's retransmission time to be configurable */
+    pcb->rto = LWIP_TCP_RTO_TIME / TCP_SLOW_INTERVAL;
+    pcb->sv = LWIP_TCP_RTO_TIME / TCP_SLOW_INTERVAL;
     pcb->rtime = -1;
     pcb->cwnd = 1;
     pcb->tmr = tcp_ticks;
