7b92080927fd30a88525375d7c5bc07d92f3662b
yuanjm <yuanjianmin@espressif.com>
2021-04-22 15:32:24 +0800

    esp-tls: Changed default behaviour for esp-tls client ( for security purpose)
    Commit ID: ca964dfb

--- components/esp-tls/esp_tls_mbedtls.c
+++ components/esp-tls/esp_tls_mbedtls.c
@@ -445,7 +445,12 @@ esp_err_t set_client_config(const char *hostname, size_t hostlen, esp_tls_cfg_t 
         return ESP_ERR_INVALID_STATE;
 #endif
     } else {
+#ifdef CONFIG_ESP_TLS_SKIP_SERVER_CERT_VERIFY
         mbedtls_ssl_conf_authmode(&tls->conf, MBEDTLS_SSL_VERIFY_NONE);
+#else
+        ESP_LOGE(TAG, "No server verification option set in esp_tls_cfg_t structure. Check esp_tls API reference");
+        return ESP_ERR_MBEDTLS_SSL_SETUP_FAILED;
+#endif
     }
 
     if (cfg->clientcert_buf != NULL && cfg->clientkey_buf != NULL) {
